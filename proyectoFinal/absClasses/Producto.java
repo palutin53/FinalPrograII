d08901faeb462521c13458c7aae5c0b7f0075d16
palutin53 <palutin31@gmail.com>
2017-03-20 15:11:42 -0600

    Commit20032017-15:11

--- proyectoFinal/absClasses/Producto.java
+++ proyectoFinal/absClasses/Producto.java
@@ -10,7 +10,76 @@ package proyectoFinal.absClasses;
  * @author Pablo Andrés Lutín Gomez - 160402009
  * @version 1.0.0
  */
+
+import proyectoFinal.Helper;
+
 public abstract class Producto
 {
+    protected String categoria;
+    protected double precio;
+    protected String nombre;
+    protected String marca;
+    protected int cantidad;
     
+    public Producto(){}
+    
+    public Producto(String categoria, double precio, String nombre, String marca, int cantidad)
+    {
+        this.categoria = categoria;
+        this.precio = precio;
+        this.nombre = nombre;
+        this.marca = marca;
+    }
+    
+    public String getCategoria()
+    {
+        return categoria;
+    }
+    
+    public void setCategoria(String categoria)
+    {
+        categoria = Helper.input("Ingrese Categoría: ");
+        this.categoria = categoria;
+    }
+    
+    public double getPrecio()
+    {
+        return precio;
+    }
+    
+    public void setPrecio()
+    {
+        precio = Double.parseDouble(Helper.input("Ingrese Precio: "));
+    }
+    
+    public String getNombre()
+    {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre)
+    {
+        nombre = Helper.input("Ingrese Nombre: ");
+        this.nombre = nombre;
+    }
+    
+    public String getMarca()
+    {
+        return marca;
+    }
+    
+    public void setMarca(String marca)
+    {
+        marca = Helper.input("Ingrese Marca: ");
+    }
+    
+    public int getCantidad()
+    {
+        return cantidad;
+    }
+    
+    public void setCantidad(int cantidad)
+    {
+        cantidad = Integer.parseInt(Helper.input("Ingrese Cantidad: "));
+    }
 }
